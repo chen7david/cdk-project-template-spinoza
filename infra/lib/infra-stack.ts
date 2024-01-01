780d629efac10decd8ab51839a6fccbe9d4d0666
David M. Chen <chen7david@me.com>
2024-01-01 07:48:32 -0300

    chore: init

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,10 +1,13 @@
 import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as apigateway from 'aws-cdk-lib/aws-apigateway'
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    /* Lambda layer */
 
     const layer = new lambda.LayerVersion(this, 'node-modules-layer', {
       layerVersionName: 'entix-node-modules-layer',
@@ -15,12 +18,25 @@ export class InfraStack extends cdk.Stack {
       description: 'lambda layer for external npm dependencies',
     })
 
+    /* Lambda */
+
     const userGetAllLambda = new lambda.Function(this, 'get-all-users', {
       functionName: 'project-name-get-all-users',
       runtime: lambda.Runtime.NODEJS_18_X,
       code: new lambda.AssetCode('assets/dist'),
       handler: 'handlers/users/get-all.handler',
       layers: [layer]
+    })
+
+    /* ApiGatewat */
+
+    const api = new apigateway.RestApi(this, 'SpinozaApigateway', {
+      restApiName: 'spinoza-apigateway',
+      description: 'exposes a rest api for Spinoza services',
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
+      }
     })
 
   }
