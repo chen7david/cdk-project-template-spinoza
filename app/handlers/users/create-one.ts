59b6d07fb793ef6b7ffcc10553ec074e092adc12
David M. Chen <chen7david@me.com>
2024-01-06 20:31:41 -0300

    Feat/add dynamo db operations (#7)
    * feat: added dynamo db actions for testing
    
    * chore: commented out test to deploy
    
    * chore: completely commeneted out test cause it's not mocked yet
    
    * chore: updated .gitignore
    
    * chore: added group_id an d user_id

--- app/handlers/users/create-one.ts
+++ app/handlers/users/create-one.ts
@@ -6,6 +6,8 @@ export const handler = async (event: APIGatewayEvent) => {
     const parsedBody = JSON.parse(event.body)
     if(!parsedBody) return errorResponse(404, 'request body is required')
     const tableName = process.env.TABLE_NAME
+    const group_id = event.pathParameters?.group_id
+    const user_id = event.pathParameters?.user_id
     const user = await UserRepository.createOne(tableName, parsedBody)
     return response(user)
 }
