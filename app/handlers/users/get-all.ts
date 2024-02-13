2d776a01f893492272100d65b5da76b54d197aae
David M. Chen <chen7david@me.com>
2024-02-13 15:26:53 -0500

    Add cognito authentication - test (#12)
    * chore: added cognito infra
    
    * chore: test one
    
    * chore: removed apiKey requirement
    
    * chore: enabled client secret
    
    * chore: added scopes
    
    * chore: added log to test ci/cd

--- app/handlers/users/get-all.ts
+++ app/handlers/users/get-all.ts
@@ -6,5 +6,6 @@ export const handler = async (event: APIGatewayEvent) => {
     const tableName = process.env.TABLE_NAME
     const users = await UserRepository.getAll(tableName)
     const jwtHeaders = event.headers['Authorization']
+    console.log({jwtHeaders})
     return response({users, jwtHeaders})
 }
